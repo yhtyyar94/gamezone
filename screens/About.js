300f0f232cca733aadcd51dcbca88467f75e4bd5
blackeagle94 <blackeagle4894@gmail.com>
2021-08-23 13:12:19 +0200

    first commit for gamezone app

---
+++ screens/About.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {StyleSheet, View, Text} from 'react-native'
+
+
+const About = () => {
+    return (
+        <View style={styles.container}>
+            <Text>About Screen</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        padding:24,
+    }
+})
+
+export default About
